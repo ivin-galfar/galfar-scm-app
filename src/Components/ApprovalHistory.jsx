bc3d47468d6b7d7c5612c194b8885b8f55034093
ivin-austan <a.ivinaustan@gmail.com>
2025-12-11 09:03:33 +0400

    fix: updated the styles in approver history

--- src/Components/ApprovalHistory.jsx
+++ src/Components/ApprovalHistory.jsx
@@ -21,21 +21,25 @@ const ApprovalHistory = () => {
 
   return (
     <div className="flex">
-      <div className="flex text-center items-start gap-5">
-        <div className="text-sm font-medium text-gray-700">Select CS NO:</div>
-        <div className="text-sm font-medium text-gray-700">
-          <select
-            className="block w-full px-2 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-sm bg-white overflow-hidden"
-            onChange={(e) => setSelectedId(e.target.value)}
-            value={selectedId || approverhistory?.id || "default"}
-          >
-            <option value="default">-Select cs-</option>
-            {allids?.map((allid) => (
-              <option key={allid.id} value={allid.id}>
-                {allid.id}
-              </option>
-            ))}
-          </select>
+      <div className="flex  items-start ">
+        <div className="flex items-center gap-5">
+          <div className="text-sm font-medium text-gray-700 text-nowrap justify-center">
+            Select CS NO:
+          </div>
+          <div className="text-sm font-medium text-gray-700">
+            <select
+              className="block w-full px-2 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-sm bg-white overflow-hidden"
+              onChange={(e) => setSelectedId(e.target.value)}
+              value={selectedId || approverhistory?.id || "default"}
+            >
+              <option value="default">-Select cs-</option>
+              {allids?.map((allid) => (
+                <option key={allid.id} value={allid.id}>
+                  {allid.id}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
       </div>
       <div className="w-full flex justify-center pt-10 gap-20">
