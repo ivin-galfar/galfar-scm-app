ae4448ebc38c5d80c143fe73bb5fd0b525420982
ivin-austan <a.ivinaustan@gmail.com>
2026-05-08 15:38:43 +0400

    fix: safely accessed the env variables

--- config/ENV.js
+++ config/ENV.js
@@ -2,6 +2,6 @@ export const REACT_SERVER_URL =
   import.meta.env.VITE_ENVIRONMENT === "development"
     ? import.meta.env.VITE_REACT_SERVER_URL
     : import.meta.env.VITE_REACT_PROD_SERVER_URL;
-export const SPECIAL_PROJECTS = import.meta.env.VITE_SPECIAL_PROJECTS.split(
+export const SPECIAL_PROJECTS = import.meta.env.VITE_SPECIAL_PROJECTS?.split(
   ",",
 ).map(Number);
